8fc91c3ba52dc5a8dc68e9e9cbd2e3ba7ec4933b
chenxin <lightnoway@qq.com>
2019-09-14 11:22:25 +0800

    第一遍，DecodeString：  消去status

--- lightnoway/src/Decode String.394.js
+++ lightnoway/src/Decode String.394.js
@@ -4,35 +4,54 @@
  * @param {string} s
  * @return {string}
  */
+// var decodeString = function (s) {
+// 	let stack = [];
+// 	let argNum = 1, argStr = "";
+// 	let status = 'str';//str
+// 	for (let c of s) {
+// 		if (c >= '0' && c <= '9') {
+// 			if (status === 'str') {
+// 				stack.push([argNum, argStr]);
+// 				status = 'num';
+// 				argNum = '';
+// 			}
+// 			argNum += c;
+// 		} else if (c === '[') {
+// 			argNum = parseInt(argNum, 10);
+// 			status = "str";
+// 			argStr = "";
+// 		} else if (c === ']') {
+// 			const str = argStr.repeat(argNum);
+// 			[argNum, argStr] = stack.pop();
+// 			argStr += str;
+// 			status = "str";
+// 		} else {
+// 			argStr += c;
+// 		}
+// 	}
+// 	return argStr;
+// };
+
+//消去status
 var decodeString = function (s) {
 	let stack = [];
-	let argNum = 1, argStr = "";
-	let status = 'str';//str
+	let argNum = "", argStr = "";
 	for (let c of s) {
-		if ('0123456789'.indexOf(c) > -1) {
-			if (status === 'str') {
-				stack.push([argNum, argStr]);
-				status = 'num';
-				argNum = '';
-			}
+		if (c >= '0' && c <= '9') {
 			argNum += c;
 		} else if (c === '[') {
-			argNum = parseInt(argNum, 10);
-			status = "str";
+			stack.push([parseInt(argNum, 10), argStr]);
+			argNum = "";
 			argStr = "";
 		} else if (c === ']') {
-			const str = argStr.repeat(argNum);
-			[argNum, argStr] = stack.pop();
-			argStr += str;
-			status = "str";
+			const [repeat, lastStr] = stack.pop();
+			argStr = lastStr + argStr.repeat(repeat); 
+			argNum = "";
 		} else {
-			if (status === 'num') {
-				argNum = 1;
-				status = "str";
-			}
 			argStr += c;
 		}
 	}
 	return argStr;
 };
+
 export default decodeString;
