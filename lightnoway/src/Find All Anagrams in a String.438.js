a4a358a1c1ea3b23e7b42fb05903bfc0fa6ecc43
chenxin <lightnoway@qq.com>
2019-09-21 17:53:39 +0800

    特征比较:特异字母数量相同

--- lightnoway/src/Find All Anagrams in a String.438.js
+++ lightnoway/src/Find All Anagrams in a String.438.js
@@ -5,14 +5,39 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function(s, p) {
+// var findAnagrams = function (s, p) {
+// 	const result = [];
+// 	const nP = p.split('').sort().join('');
+// 	for (let i = 0, len = s.length - p.length; i <= len; i++) {
+// 		if (nP === s.substr(i, nP.length).split('').sort().join('')) {
+// 			result.push(i);
+// 		}
+// 	}
+// 	return result;
+// };
+
+//特征比较:特异字母数量相同
+var findAnagrams = function (s, p) {
 	const result = [];
-	const nP=p.split('').sort().join('');
-	for(let i=0,len = s.length - p.length;i<=len;i++){
-		if(nP === s.substr(i,nP.length).split('').sort().join('')){
+	const hashS = Object.create(null);
+	const hashP = Object.create(null);
+	for (let i = 'a'; i <= 'z'; i = String.fromCharCode(i.charCodeAt(0) + 1)) {
+		hashS[i] = 0;
+		hashP[i] = 0;
+	}
+	for (let i = 0; i < p.length; i++) {
+		hashP[p[i]]++;
+		hashS[s[i]]++;
+	}
+	const nP = JSON.stringify(hashP);
+	for (let i = 0, len = s.length - p.length; i <= len; i++) {
+		if (JSON.stringify(hashS) === nP) {
 			result.push(i);
 		}
+		hashS[s[i]]--;
+		hashS[s[i + p.length]]++;
 	}
 	return result;
-}; 
+};
+
 export default findAnagrams;
