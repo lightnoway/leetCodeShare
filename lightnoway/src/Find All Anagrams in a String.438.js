1769599687ac899543ee5a00d802ec3a15dc304b
chenxin <lightnoway@qq.com>
2019-09-22 11:11:48 +0800

    特征比较改进，滑动窗口

--- lightnoway/src/Find All Anagrams in a String.438.js
+++ lightnoway/src/Find All Anagrams in a String.438.js
@@ -17,33 +17,70 @@
 // };
 
 //特征比较:特异字母数量相同
+// var findAnagrams = function (s, p) {
+// 	const result = [];
+// 	const hashS = Object.create(null);
+// 	const hashP = Object.create(null);
+// 	for (let i = 'a'; i <= 'z'; i = String.fromCharCode(i.charCodeAt(0) + 1)) {
+// 		hashS[i] = 0;
+// 		hashP[i] = 0;
+// 	}
+// 	for (let i = 0; i < p.length; i++) {
+// 		hashP[p[i]]++;
+// 		hashS[s[i]]++;
+// 	} 
+// 	for (let i = 0, len = s.length - p.length; i <= len; i++) {
+// 		if (objEqual(hashS,hashP)) {
+// 			result.push(i);
+// 		}
+// 		hashS[s[i]]--;
+// 		hashS[s[i + p.length]]++;
+// 	}
+// 	return result;
+// };
+// function objEqual(o1, o2) {
+// 	for (let key in o1) {
+// 		if (o2[key] !== o1[key]) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+//特征比较改进，滑动窗口
+//注意matchCount
 var findAnagrams = function (s, p) {
 	const result = [];
-	const hashS = Object.create(null);
 	const hashP = Object.create(null);
-	for (let i = 'a'; i <= 'z'; i = String.fromCharCode(i.charCodeAt(0) + 1)) {
-		hashS[i] = 0;
-		hashP[i] = 0;
+	const pLen = p.length
+		, sLen = s.length;
+	for (let char of p) {
+		hashP[char] = (hashP[char] || 0) + 1;
 	}
-	for (let i = 0; i < p.length; i++) {
-		hashP[p[i]]++;
-		hashS[s[i]]++;
-	} 
-	for (let i = 0, len = s.length - p.length; i <= len; i++) {
-		if (objEqual(hashS,hashP)) {
-			result.push(i);
+	let left = 0
+		, right = 0
+		, matchCount = 0;
+	while (right < sLen) {
+		let rightChar = s[right++];
+		if (rightChar in hashP) {
+			if (--hashP[rightChar] >= 0) {
+				matchCount++;
+			}
 		}
-		hashS[s[i]]--;
-		hashS[s[i + p.length]]++;
+
+		while (matchCount === pLen) {
+			if (right - left === pLen) {
+				result.push(left);
+			}
+			let leftChar = s[left++];
+			if (leftChar in hashP) {
+				if (hashP[leftChar]++ >= 0) {
+					matchCount--;
+				}
+			}
+		}
 	}
+
 	return result;
 };
-function objEqual(o1,o2){
-	for(let key in o1){
-		if(o2[key]!==o1[key]){
-			return false;
-		}
-	}
-	return true;
-}
 export default findAnagrams;
--- lightnoway/test/Find All Anagrams in a String.438.js
+++ lightnoway/test/Find All Anagrams in a String.438.js
@@ -2,12 +2,9 @@ import fn from '../src/Find All Anagrams in a String.438';
 
 test('Find All Anagrams in a String.438', () => {
 	const test = [
-		["3[a]2[bc]", "aaabcbc"],
-		["3[a2[c]]", "accaccacc"],
-		["2[abc]3[cd]ef", "abcabccdcdcdef"],
-		["3[a2[c]bc]", "accbcaccbcaccbc"],
+		["cbaebabacd", "abc",[]] 
 	]
 	for (let t of test) {
-		expect(fn(t[0])).toEqual(t[1]);
+		expect(fn(t[0],t[1])).toEqual(t[2]);
 	}
 });
