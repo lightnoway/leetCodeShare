dc6e4ae2da60e2b3652942d3ef53971000b7dc90
chenxin <lightnoway@qq.com>
2019-09-22 11:51:21 +0800

    JSON.stringify 比较 Anagrams

---
+++ lightnoway/src/Group Anagrams.49.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) { 
+	const groups = new Map() ;
+	for(const str of strs)  {
+		const hash = getHash(str);
+		if(!groups.has(hash)){
+			groups.set(hash,[])
+		}
+		groups.get(hash).push(str);
+	}
+	const ret = []   ;
+	for(let [,group] of groups){
+		ret.push(group);
+	}
+	return ret;
+};
+function getHash(str){
+	const hash = Object.create(null);
+	for(let c of 'abcdefghijklmnopqrstuvwxyz'){
+		hash[c]=0;
+	}
+	for(let c of str){
+		hash[c]++
+	}
+	return  JSON.stringify(hash);
+}
